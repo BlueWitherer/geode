2f022c00525c79132a1275506e4f759a949eb24d
Cheeseworks <bluewitherer2422@gmail.com>
2026-08-27 11:04:12 -0500

    initial impl

---
+++ loader/include/Geode/ui/BoxedLabel.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <Geode/DefaultInclude.hpp>
+#include <Geode/ui/Label.hpp>
+#include <Geode/ui/NineSlice.hpp>
+#include <cocos2d.h>
+
+namespace geode {
+    class GEODE_DLL BoxedLabel final : public cocos2d::CCNode {
+        class Impl;
+        std::unique_ptr<Impl> m_impl;
+
+    protected:
+        BoxedLabel();
+        ~BoxedLabel();
+
+        void resize();
+
+        bool init(std::string text, ZStringView font, float width);
+
+    public:
+        /// Create a label visually encased by a NineSlice with dynamic width
+        /// @param text Text to display on the label
+        /// @param font Name of the label's font `.fnt` file
+        static BoxedLabel* create(std::string text, ZStringView font);
+
+        /// Create a label visually encased by a NineSlice with a width limit
+        /// @param text Text to display on the label
+        /// @param font Name of the label's font `.fnt` file
+        /// @param width Text label width limit
+        static BoxedLabel* create(std::string text, ZStringView font, float width);
+
+        /// Change the label text
+        /// @param text Text to display on the label
+        void setText(std::string text);
+
+        /// Change the label text
+        /// @param font Name of the label's font `.fnt` file
+        void setFont(ZStringView font);
+
+        /// Change the scale of the text label and re-size the box with it
+        /// @param width Scale for the label
+        void setTextScale(float scale);
+
+        /// Change the label's text alignment
+        /// @param alignment Direction the label expands from
+        void setTextAlignment(Label::Alignment alignment);
+
+        /// Set the maximum width of the label
+        void setMaxLabelWidth(float width);
+
+        /// Change the color of the NineSlice surrounding the text label
+        void setBGColor(cocos2d::ccColor3B const& color);
+
+        /// Change the opacity of the NineSlice surrounding the text label
+        void setBGOpacity(GLubyte opacity);
+
+        /// Lock the box at the maximum width if set, instead of dynamically expanding with the label
+        void setLockBGWidth(bool lock);
+
+        float getMaxLabelWidth() const noexcept;
+        bool isBGWidthLocked() const noexcept;
+
+        Label* getLabelNode() const noexcept;
+        NineSlice* getBGSprite() const noexcept;
+    };
+};
---
+++ loader/src/ui/nodes/BoxedLabel.cpp
@@ -0,0 +1,120 @@
+#include <Geode/ui/BoxedLabel.hpp>
+#include <Geode/utils/cocos.hpp>
+
+using namespace geode::prelude;
+
+class BoxedLabel::Impl final {
+public:
+    float m_width = 0.f;
+    bool m_lockWidth = false;
+
+    Label* m_label = nullptr;
+    NineSlice* m_bg = nullptr;
+};
+
+BoxedLabel::BoxedLabel() : m_impl(std::make_unique<Impl>()) {};
+BoxedLabel::~BoxedLabel() {};
+
+bool BoxedLabel::init(std::string text, ZStringView font, float width) {
+    if (!CCNode::init()) return false;
+
+    m_impl->m_width = width;
+
+    this->setAnchorPoint({0.5f, 0.5f});
+
+    m_impl->m_label = Label::createRich(std::move(text), font);
+    m_impl->m_label->setZOrder(1);
+    m_impl->m_label->setAlignment(Label::Alignment::Center);
+
+    if (width > 0.f) m_impl->m_label->setMaxWidth(width);
+
+    m_impl->m_bg = NineSlice::create("white-square.png"_spr);
+    m_impl->m_bg->setOpacity(100);
+    m_impl->m_bg->setColor({0, 0, 0});
+
+    this->resize();
+
+    this->addChildAtPosition(m_impl->m_label, Anchor::Center, {});
+    this->addChildAtPosition(m_impl->m_bg, Anchor::Center, {});
+
+    return true;
+};
+
+void BoxedLabel::resize() {
+    this->setContentSize({
+        (m_impl->m_lockWidth ? m_impl->m_width : m_impl->m_label->getScaledContentWidth()) + 8.75f,
+        m_impl->m_label->getScaledContentHeight() + 5.f,
+    });
+    m_impl->m_bg->setContentSize(this->getScaledContentSize());
+    this->updateLayout();
+};
+
+void BoxedLabel::setText(std::string text) {
+    m_impl->m_label->setText(std::move(text));
+    this->resize();
+};
+
+void BoxedLabel::setFont(ZStringView font) {
+    m_impl->m_label->setFont(font);
+    this->resize();
+};
+
+void BoxedLabel::setTextScale(float scale) {
+    m_impl->m_label->setScale(scale);
+    this->resize();
+};
+
+void BoxedLabel::setTextAlignment(Label::Alignment alignment) {
+    m_impl->m_label->setAlignment(alignment);
+};
+
+void BoxedLabel::setMaxLabelWidth(float width) {
+    m_impl->m_width = width;
+    m_impl->m_label->setMaxWidth(width);
+
+    this->resize();
+};
+
+void BoxedLabel::setBGColor(ccColor3B const& color) {
+    m_impl->m_bg->setColor(color);
+};
+
+void BoxedLabel::setBGOpacity(GLubyte opacity) {
+    m_impl->m_bg->setOpacity(opacity);
+};
+
+void BoxedLabel::setLockBGWidth(bool lock) {
+    m_impl->m_lockWidth = lock;
+    this->resize();
+};
+
+float BoxedLabel::getMaxLabelWidth() const noexcept {
+    return m_impl->m_width;
+};
+
+bool BoxedLabel::isBGWidthLocked() const noexcept {
+    return m_impl->m_lockWidth;
+};
+
+Label* BoxedLabel::getLabelNode() const noexcept {
+    return m_impl->m_label;
+};
+
+NineSlice* BoxedLabel::getBGSprite() const noexcept {
+    return m_impl->m_bg;
+};
+
+BoxedLabel* BoxedLabel::create(std::string text, ZStringView font) {
+    return BoxedLabel::create(std::move(text), font, 0.f);
+};
+
+BoxedLabel* BoxedLabel::create(std::string text, ZStringView font, float width) {
+    auto ret = new BoxedLabel();
+    if (ret->init(std::move(text), font, width)) {
+        ret->autorelease();
+        return ret;
+    };
+
+    delete ret;
+    return nullptr;
+};
